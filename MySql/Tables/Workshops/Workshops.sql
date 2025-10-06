730cdee82def8c9d25c4b2856521d9e9e7bc04d8
DiAnGaMa18 <dixongaitan3333@gmail.com>
2025-10-05 20:42:38 -0600

    Actualizacion de tabla Workshop

--- MySql/Tables/Workshops/Workshops.sql
+++ MySql/Tables/Workshops/Workshops.sql
@@ -1,9 +1,13 @@
 CREATE TABLE workshops (
-    id VARCHAR(50) PRIMARY KEY,
-    title VARCHAR(200) NOT NULL,
-    description TEXT NOT NULL,
-    imageUrl VARCHAR(255) NOT NULL,
-    objectives TEXT NOT NULL,  -- Store as JSON string
-    materials TEXT NOT NULL,   -- Store as JSON string
-    learnText TEXT NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    ubicacion VARCHAR(255),
+    descripcion TEXT, 
+    imagen VARCHAR(255),
+    materials TEXT,
+    aprender TEXT,
+    fecha DATE,
+    hora TIME,
+    capacidad INT
 );
+

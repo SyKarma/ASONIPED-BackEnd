350ca269f949a987766c4dbbd953b387ab972294
SyKarma <102105983+SyKarma@users.noreply.github.com>
2026-05-02 19:35:24 -0600

    Done

--- MySql/migrations/20260201_personal_data_family_occupations.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE personal_data
-  ADD COLUMN mother_occupation VARCHAR(255) NULL AFTER mother_phone,
-  ADD COLUMN father_occupation VARCHAR(255) NULL AFTER father_phone,
-  ADD COLUMN legal_guardian_occupation VARCHAR(255) NULL AFTER legal_guardian_phone;
